2ee5afb8681ad765c62ce78977819e1db5a2aa70
vysakhtu <ysakhtu@gmail.com>
2024-09-01 09:57:04 +0100

    Delete pet/PTProject/myproject directory

--- pet/PTProject/myproject/pet_store/petsapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PetUser,Pet
-# Register your models here.
-# class PetAdmin(admin.ModelAdmin):
-#     list_display=('id','name','gender','species','breed','age','description')
-# admin.site.register(Pet,PetAdmin)
-admin.site.register(Pet)
-admin.site.register(PetUser)
--- pet/PTProject/myproject/pet_store/petsapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PetsappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "petsapp"
